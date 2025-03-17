a12788d134f932062d3a8141da29465eaabe9c2e
RadialistaEdsonLeite <156628216+RadialistaEdsonLeite@users.noreply.github.com>
2025-03-17 18:55:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const q = questions[currentQuestionIndex];
     audio.src = q.song;
-    questionText.textContent = "Qual é a música?";
+    questionText.textContent = "Ouça e responda!";
     optionsContainer.innerHTML = "";
 
     q.options.forEach((option) => {
